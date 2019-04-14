1cf9c6305c509192f4d590d4a2907f371dc38093
keshab <self@keshab.net>
2019-04-15 00:46:44 +0200

    move generator to index

--- index.py
+++ index.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 from collections import namedtuple
+from string import Template
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+
+
+Link = namedtuple('Link', ['url', 'description'])
+Work = namedtuple('Work', ['company', 'url', 'start', 'end'])
+Tech = namedtuple('Tech', ['title', 'type', 'skill_level', 'more'])
 
 
 about = {
@@ -8,51 +17,54 @@ about = {
     'timezone': 'Europe/Berlin',
     'birth_timezone': 'Asia/Kathmandu',
     'description': 'software development and Lowercase',
+    'links': [
+        Link('mailto:self@<this-domain>', 'Email'),
+        Link('https://keshab.net', 'Website'),
+        Link('https://github.com/poudel', 'Github'),
+    ],
+    'works': [
+        Work('Seerene', 'https://seerene.com', 'Dec 2018', None),
+        Work('Khalti', 'https://khalti.com', 'Sep 2017', 'Nov 2018'),
+        Work('AayuLogic', 'https://aayulogic.com', 'Jan 2017', 'Sep 2017'),
+        Work('Real Solutions', 'https://realsolutions.com.np', 'May 2015', 'Dec 2016'),
+    ],
+    'tech': [
+        Tech('Python', 'language', 'pretty_good', 'bread and butter'),
+        Tech('Django/DRF/Flask', 'web_framework', 'pretty_good', 'bread and butter'),
+
+        Tech('PostgreSQL', 'database', 'good_enough', 'it stares back'),
+        Tech('MySQL', 'database', 'good_enough', 'use postgres'),
+
+        Tech('Emacs', 'operating_system', 'pretty_good', 'work environment of choice'),
+        Tech('ArchLinux', 'bootloader', 'good', 'work environment'),
+
+        Tech('JavaScript', 'sicilian_bull', 'pretty_good', 'find someone else'),
+        Tech('PHP', '', '', ''),
+    ],
+    'keywords': [
+        'Go', 'Elixir', 'Erlang', 'Emacs Lisp', 'Elm',
+        'TypeScript', 'Ubuntu', 'Debian', 'CentOS',
+    ],
 }
 
 
-Link = namedtuple('Link', ['url', 'description'])
+def generate(about):
+    formatter = HtmlFormatter(linenos='inline', style='gruvbox')
 
-about['links'] = [
-    Link('mailto:self@<this-domain>', 'Email'),
-    Link('https://keshab.net', 'Website'),
-    Link('https://github.com/poudel', 'Github'),
-]
+    with open('index.py') as codefile:
+        code = codefile.read()
 
+    with open('index_tpl.html') as tpl:
+        template = Template(tpl.read())
 
-Work = namedtuple('Work', ['company', 'url', 'start', 'end'])
+    about['style'] = formatter.get_style_defs()
+    about['body'] = highlight(code, PythonLexer(), formatter)
 
-about['works'] = [
-    Work('Seerene', 'https://seerene.com', 'Dec 2018', None),
-    Work('Khalti', 'https://khalti.com', 'Sep 2017', 'Nov 2018'),
-    Work('AayuLogic', 'https://aayulogic.com', 'Jan 2017', 'Sep 2017'),
-    Work('Real Solutions', 'https://realsolutions.com.np', 'May 2015', 'Dec 2016'),
-]
+    index_html = template.substitute(about)
 
-
-Tech = namedtuple('Tech', ['title', 'type', 'skill_level', 'more'])
-
-about['tech'] = [
-    Tech('Python', 'language', 'pretty_good', 'bread and butter'),
-    Tech('Django/DRF/Flask', 'web_framework', 'pretty_good', 'bread and butter'),
-
-    Tech('PostgreSQL', 'database', 'good_enough', 'it stares back'),
-    Tech('MySQL', 'database', 'good_enough', 'use postgres'),
-
-    Tech('Emacs', 'operating_system', 'pretty_good', 'work environment of choice'),
-    Tech('ArchLinux', 'bootloader', 'good', 'work environment'),
-
-    Tech('JavaScript', 'sicilian_bull', 'pretty_good', 'find someone else'),
-    Tech('PHP', '', '', ''),
-]
-
-
-about['keywords'] = [
-    'Go', 'Elixir', 'Erlang', 'Emacs Lisp', 'Elm', 'TypeScript',
-    'Ubuntu', 'Debian', 'CentOS',
-]
+    with open('index.html', 'w') as index_html_file:
+        index_html_file.write(index_html)
 
 
 if __name__ == '__main__':
-    from generator import generate
     generate(about)
